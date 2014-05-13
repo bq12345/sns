d0cc057f9953b5e577b57d1a05e3875dd59c80ad
bq12345 <1075707996@qq.com>
2014-05-13 15:16:48 +0800

    update

---
+++ src/org/sns/util/SessionManager.java
@@ -0,0 +1,48 @@
+package org.sns.util;
+
+import org.sns.model.User;
+
+import com.opensymphony.xwork2.ActionContext;
+
+public class SessionManager {
+	/**
+	 * If a user is login
+	 * @return boolean
+	 */
+	public static boolean isOnLine() {
+		return null == getUser();
+	}
+
+	/**
+	 * Remove user&key from session
+	 */
+	public static void remove() {
+		ActionContext.getContext().getSession().remove("user");
+		ActionContext.getContext().getSession().remove("key");
+	}
+
+	/**
+	 * Put user to session
+	 * @param u
+	 */
+	public static void add(User u) {
+		ActionContext.getContext().getSession().put("user", u.getUsername());
+		ActionContext.getContext().getSession().put("key", u.getUserId());
+	}
+
+	/**
+	 * Get a user from session
+	 * @return user
+	 */
+	public static User getUser() {
+		return (User) ActionContext.getContext().getSession().get("user");
+	}
+
+	/**
+	 * Get a key from session
+	 * @return
+	 */
+	public static int getKey() {
+		return (Integer) ActionContext.getContext().getSession().get("key");
+	}
+}
